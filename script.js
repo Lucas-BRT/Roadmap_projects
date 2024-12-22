aef65ada983f04b53bed966cc3bb9fc20e98acb3
lucasBRT <lucasbritodepaula.t@gmail.com>
2024-12-22 17:14:31 -0300

    refac: usa Early Return para aumentar a legibilidade

--- script.js
+++ script.js
@@ -9,38 +9,40 @@ convertButton.addEventListener('click', () =>{
     const rawUnity = rawOption.value;
     const convertUnity = convertOption.value;
 
-    if ( rawUnity != convertUnity){
-        if(rawUnity == 'celsius' && convertUnity == 'fairenheit'){
-            let typedunity = Number(document.getElementById('rawunity').value);
-            let convert  = (typedunity*1.8)+32;
-            convertedUnity.value = convert.toFixed(1);
-        } 
-        if(rawUnity == 'fairenheit' && convertUnity == 'celsius'){
-            let typedunity = Number(document.getElementById('rawunity').value);
-            let convert  = (typedunity-32)/1.8;
-            convertedUnity.value = convert.toFixed(1);
-        } 
-        if(rawUnity == 'celsius' && convertUnity == 'kelvin'){
-            let typedunity = Number(document.getElementById('rawunity').value);
-            let convert  = typedunity+273.15;
-            convertedUnity.value = convert.toFixed(2);
-        } 
-        if(rawUnity == 'fairenheit' && convertUnity == 'kelvin'){
-            let typedunity = Number(document.getElementById('rawunity').value);
-            let convert  = (((typedunity)+459.67)*5)/9;
-            convertedUnity.value = convert.toFixed(2);
-        }
-        if(rawUnity == 'kelvin' && convertUnity == 'fairenheit'){
-            let typedunity = Number(document.getElementById('rawunity').value);
-            let convert  = ((typedunity-273.15)*1.8)+32;
-            convertedUnity.value = convert.toFixed(2);
-        }
-        if(rawUnity == 'kelvin' && convertUnity == 'celsius'){
-            let typedunity = Number(document.getElementById('rawunity').value);
-            let convert  = typedunity-273.15;
-            convertedUnity.value = convert.toFixed(2);
-        } 
-    }else{
+    if (rawUnity === convertUnity) {
         alert('Please select a diferent type of unity!!!')
+        return
     }
+
+    if(rawUnity == 'celsius' && convertUnity == 'fairenheit'){
+        let typedunity = Number(document.getElementById('rawunity').value);
+        let convert  = (typedunity*1.8)+32;
+        convertedUnity.value = convert.toFixed(1);
+    } 
+    if(rawUnity == 'fairenheit' && convertUnity == 'celsius'){
+        let typedunity = Number(document.getElementById('rawunity').value);
+        let convert  = (typedunity-32)/1.8;
+        convertedUnity.value = convert.toFixed(1);
+    } 
+    if(rawUnity == 'celsius' && convertUnity == 'kelvin'){
+        let typedunity = Number(document.getElementById('rawunity').value);
+        let convert  = typedunity+273.15;
+        convertedUnity.value = convert.toFixed(2);
+    } 
+    if(rawUnity == 'fairenheit' && convertUnity == 'kelvin'){
+        let typedunity = Number(document.getElementById('rawunity').value);
+        let convert  = (((typedunity)+459.67)*5)/9;
+        convertedUnity.value = convert.toFixed(2);
+    }
+    if(rawUnity == 'kelvin' && convertUnity == 'fairenheit'){
+        let typedunity = Number(document.getElementById('rawunity').value);
+        let convert  = ((typedunity-273.15)*1.8)+32;
+        convertedUnity.value = convert.toFixed(2);
+    }
+    if(rawUnity == 'kelvin' && convertUnity == 'celsius'){
+        let typedunity = Number(document.getElementById('rawunity').value);
+        let convert  = typedunity-273.15;
+        convertedUnity.value = convert.toFixed(2);
+    } 
+
 })
