ecf4cfc5f1d1e4afbf633de0e31e0c1810ac206c
lucasBRT <lucasbritodepaula.t@gmail.com>
2024-12-22 16:14:35 -0300

    fix: renomeia o campo de unidade convertida

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const convertButton = document.getElementById('convertBTN');
-const convertedunity = document.getElementById('convertedunity');
+const convertedUnity = document.getElementById('convertedUnity');
 
 convertButton.addEventListener('click', () =>{
     const rawOption = document.getElementById('unityOptions');
@@ -13,32 +13,32 @@ convertButton.addEventListener('click', () =>{
         if(rawUnity == 'celsius' && convertUnity == 'fairenheit'){
             let typedunity = Number(document.getElementById('rawunity').value);
             let convert  = (typedunity*1.8)+32;
-            convertedunity.value = convert.toFixed(1);
+            convertedUnity.value = convert.toFixed(1);
         } 
         if(rawUnity == 'fairenheit' && convertUnity == 'celsius'){
             let typedunity = Number(document.getElementById('rawunity').value);
             let convert  = (typedunity-32)/1.8;
-            convertedunity.value = convert.toFixed(1);
+            convertedUnity.value = convert.toFixed(1);
         } 
         if(rawUnity == 'celsius' && convertUnity == 'kelvin'){
             let typedunity = Number(document.getElementById('rawunity').value);
             let convert  = typedunity+273.15;
-            convertedunity.value = convert.toFixed(2);
+            convertedUnity.value = convert.toFixed(2);
         } 
         if(rawUnity == 'fairenheit' && convertUnity == 'kelvin'){
             let typedunity = Number(document.getElementById('rawunity').value);
             let convert  = (((typedunity)+459.67)*5)/9;
-            convertedunity.value = convert.toFixed(2);
+            convertedUnity.value = convert.toFixed(2);
         }
         if(rawUnity == 'kelvin' && convertUnity == 'fairenheit'){
             let typedunity = Number(document.getElementById('rawunity').value);
             let convert  = ((typedunity-273.15)*1.8)+32;
-            convertedunity.value = convert.toFixed(2);
+            convertedUnity.value = convert.toFixed(2);
         }
         if(rawUnity == 'kelvin' && convertUnity == 'celsius'){
             let typedunity = Number(document.getElementById('rawunity').value);
             let convert  = typedunity-273.15;
-            convertedunity.value = convert.toFixed(2);
+            convertedUnity.value = convert.toFixed(2);
         } 
     }else{
         alert('Please select a diferent type of unity!!!')
